7ed462470987869646258c5df7f60a3feccff5c8
muthukumarankvignesh <61156269+muthukumarankvignesh@users.noreply.github.com>
2020-07-23 15:32:24 +0530

    Add files via upload

---
+++ Cachematrix_programming_assignment_2.R
@@ -0,0 +1,34 @@
+## Make cache matrix
+makeCacheMatrix <- function( m = matrix() ) {
+  i <- NULL
+  set <- function( matrix ) {
+    m <<- matrix
+    i <<- NULL
+  }
+  get <- function() {
+    m
+  }
+  setInverse <- function(inverse) {
+    i <<- inverse
+  }
+  getInverse <- function() {
+    i
+  }
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+
+## Cache solve
+cacheSolve <- function(x, ...) {
+  m <- x$getInverse()
+  if( !is.null(m) ) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data) %*% data
+  x$setInverse(m)
+  m
+}
